ad2db05deaeb7a8fbf11d993f4e00ff3d2862be8
helenetz <zthelene@gmail.com>
2025-05-31 09:44:36 +0200

    v4

--- script.js
+++ script.js
@@ -37,6 +37,10 @@ function getData() {
         console.log(nomJournal);
         navConteneur.appendChild(nomJournal)
 
+        data.journal.themes.forEach(theme => {
+          
+        });
+
         mysteres = document.createElement("a");
         mysteres.className = "mysteres"
         mysteres.textContent = data.journal.themes[0].nom;
@@ -46,7 +50,7 @@ function getData() {
 
         
         occulte = document.createElement("a");
-        occulte.className = "occulte"
+        occulte.className= "occulte"
         occulte.textContent = data.journal.themes[1].nom;
         //console.log(data.journal.themes[1]);
         occulte.href ="#"
@@ -54,8 +58,7 @@ function getData() {
         navConteneur.appendChild(occulte)
 
         arts = document.createElement("a");
-        arts.className = arts
-        // divNav.classList.add("arts");
+        arts.className = "arts"
         arts.textContent = data.journal.themes[4].nom;
         //console.log(data.journal.themes[1]);
         arts.href ="#"
@@ -63,9 +66,8 @@ function getData() {
         navConteneur.appendChild(arts)
 
         fiction = document.createElement("a");
-        // fiction.classList = fiction
-        // divNav.classList.add("fiction");
-        fiction.textContent = data.journal.themes[1].nom;
+        fiction.className = "fiction"
+        fiction.textContent = data.journal.themes[5].nom;
         //console.log(data.journal.themes[1]);
         fiction.href ="#"
         console.log(fiction);
@@ -89,167 +91,17 @@ function getData() {
         afficherAutresArticles(data);
         afficherAuteurs(data);
 
-        /*
-        let headerConteneur = document.getElementById("headerConteneur")
-        let phraseAccroche = document.getElementById("phraseAccroche")
-     
-      
+ 
+        divAuteurs = document.createElement("h2");
+        divAuteurs.classList.add = "divAuteurs";
 
-        phraseAccroche = document.createElement("h3");
-        phraseAccroche.textContent = data.journal.phraseAccroche;
-        console.log(phraseAccroche);
-        headerConteneur.appendChild(phraseAccroche)
+        let titreEquipe = document.getElementById("titreEquipe")
+        titreEquipe.textContent = "Découvrez L'équipe D'Arcana Hebdo !"
+        divAuteurs.appendChild(titreEquipe)
 
-        let indexThemes = data.journal.themes;
-        console.log(indexThemes);
-        
-        let divThemes = document.createElement("div")
-        divThemes.className = "divThemes"
-
-        indexThemes.forEach(theme => {
-        let carteTheme = document.createElement("div")
-        carteTheme.className = "carteTheme"
-        
-       
-        let titreTheme = document.createElement("h2")
-        titreTheme.className = "titreTheme"
-        titreTheme.textContent = theme.nom
-        carteTheme.appendChild(titreTheme)
-        
-        descriptionTheme = document.createElement("p")
-        descriptionTheme.className = "descriptionTheme"
-        descriptionTheme.className="descriptionTheme"
-        descriptionTheme.textContent = theme.description
-        carteTheme.appendChild(descriptionTheme)
-        
-        divThemes.appendChild(carteTheme)
-        headerConteneur.appendChild(divThemes)
-
-        });
-        
-       
-        let articleRecentConteneur = document.getElementById("articleRecentConteneur")
-         let articlePrincipal = data.journal.articlePrincipal;
-
-        let TitreArticleP = document.createElement("h2")
-        TitreArticleP.className = "TitreArticleP"
-        TitreArticleP.textContent = articlePrincipal.titre
-        articleRecentConteneur.appendChild(TitreArticleP)
-
-        let imgArticleP = document.createElement("img")
-         imgArticleP.className = " imgArticleP"
-         imgArticleP.src = articlePrincipal.image
-         imgArticleP.alt = 
-        articleRecentConteneur.appendChild( imgArticleP)
-
-        let dateArticleP = document.createElement("p")
-        dateArticleP.className = "dateArticleP"
-        dateArticleP.textContent = 
-        dateArticleP.textContent = `${articlePrincipal.date} - ${articlePrincipal.theme}`
-        articleRecentConteneur.appendChild(dateArticleP)
-        
-
-        let descriptionArticle = document.createElement("p")
-        descriptionArticle.className = "descriptionArticle"
-        descriptionArticle.textContent = articlePrincipal.description
-        articleRecentConteneur.appendChild(descriptionArticle)
-        
-        let btnLireP = document.createElement("button");
-         btnLireP.textContent = "Lire l'article";
-         btnLireP.className = "btn-lire-article";
-        articleRecentConteneur.appendChild(btnLireP);
-
-
-
-
-        let autresConteneurs = document.getElementById("autresConteneurs")
-        let indexArticles = data.journal.articles
-        let divArticles = document.createElement("div")
-        divArticles.className = "divArticles"
-
-
-
-         indexArticles.forEach(article => {
-        
-        let carteArticles = document.createElement("div")
-        carteArticles.className = "carteArticles"
-
-        let titreArticlesAutre = document.createElement("h3")
-         titreArticlesAutre.className = " titreArticlesAutre"
-         titreArticlesAutre.textContent = article.titre
-        carteArticles.appendChild( titreArticlesAutre)
-
-        let imgArticlesAutre = document.createElement("img")
-        imgArticlesAutre.className = "imgArticlesAutre"
-        imgArticlesAutre.src = article.image
-        imgArticlesAutre.alt = article.titre
-        carteArticles.appendChild
-        (imgArticlesAutre)
-
-
-        let dateArticlesAutre = document.createElement("p")
-        dateArticlesAutre.className = "dateArticlesAutre"
-        dateArticlesAutre.textContent = `${article.date} - ${article.theme}`
-        carteArticles.appendChild(dateArticlesAutre)
-
-        let btnLire = document.createElement("button");
-         btnLire.textContent = "Lire l'article";
-         btnLire.className = "btn-lire-article";
-        carteArticles.appendChild(btnLire);
-
-        divArticles.appendChild(carteArticles)
-       
-
-         //console.log(autresConteneurs);
-       
-      
-
-
-        });
-
-         autresConteneurs.appendChild(divArticles)  
-
-
-        let indexAuteur = data.journal.auteurs
-        let divAuteurs = document.createElement("div")
-        divAuteurs.className = "divAuteurs"
-        
-        indexAuteur.forEach(auteur => {
-        
-        let carteAuteur = document.createElement("div")
-        carteAuteur.className = "carteAuteur"
-
-        let avatarAuteurs = document.createElement("img")
-        avatarAuteurs.className="avatarAuteurs"
-        avatarAuteurs.src = data.journal.avatar
-        avatarAuteurs.alt = "avatar de l'auteur"
-        console.log(avatar);
-        carteAuteur.appendChild(avatarAuteurs)
-
-        let prenomAuteurs = document.createElement("p")
-        prenomAuteurs.className = "prenomAuteurs"
-        prenomAuteurs.textContent = auteur.prenom
-        carteAuteur.appendChild(prenomAuteurs)
-
-        let presentationAuteurs = document.createElement("p")
-        presentationAuteurs.className = "presentationAuteurs"
-        presentationAuteurs.textContent = auteur.presentation
-        carteAuteur.appendChild(presentationAuteurs)
-
-        let typeExperienceAuteurs = document.createElement("p")
-        typeExperienceAuteurs.className = "typeExperienceAuteurs"
-        typeExperienceAuteurs.textContent = auteur.typeExperience
-        carteAuteur.appendChild(typeExperienceAuteurs)
-
-        divAuteurs.appendChild(carteAuteur)
+        console.log(titreEquipe);
         autresConteneurs.appendChild(divAuteurs)
         
- 
-        
-        
-        });
-        
-*/
 
         let footerConteneur = document.getElementById("footerConteneur")
         let footerDiv = document.createElement("p")
@@ -272,6 +124,7 @@ function getData() {
  }
  
  getData();
+
 
  ///ON écrit les fonctions ici
 
@@ -372,18 +225,20 @@ function afficherAutresArticles(data) {
   autresConteneurs.appendChild(divArticles);
 }
 
+
 function afficherAuteurs(data) {
   let autresConteneurs = document.getElementById("autresConteneurs");
   let divAuteurs = document.createElement("div");
   divAuteurs.className = "divAuteurs";
+  let indexAuteurs = data.journal.auteurs
 
-  data.journal.auteurs.forEach(auteur => {
+  indexAuteurs.forEach(auteur => {
     let carte = document.createElement("div");
     carte.className = "carteAuteur";
 
     let avatar = document.createElement("img");
     avatar.className = "avatarAuteurs";
-    avatar.src = data.journal.avatar;
+    avatar.src = auteur.avatar;
     avatar.alt = "avatar de l'auteur";
     carte.appendChild(avatar);
 
@@ -407,6 +262,7 @@ function afficherAuteurs(data) {
 
   autresConteneurs.appendChild(divAuteurs);
 }
+
 
 
 /*
